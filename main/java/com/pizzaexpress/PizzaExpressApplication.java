2f385de0b41c9aab786c5b4fdf53979ebee80f7c
hsnahim <hsnahim@gmail.com>
2025-05-09 14:49:31 -0300

    back pronto

--- main/java/com/pizzaexpress/PizzaExpressApplication.java
+++
@@ -1,6 +0,0 @@
-@SpringBootApplication
-public class PizzaExpressApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(PizzaExpressApplication.class, args);
-    }
-}
